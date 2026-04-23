3d24dd100b989cfd355563c79b983426bc753fe6
MUYU_Twilighter <1484605372@qq.com>
2026-04-23 10:15:46 +0800

    fix root

--- docs.config.mjs
+++ docs.config.mjs
@@ -8,7 +8,7 @@ export const locales = {
   }
 }
 
-export const siteOrigin = 'https://muyutwilighter.github.io'
+export const siteOrigin = 'https://croparia.muyucloud.cool/'
 export const siteBase = '/'
 export const siteUrl = `${siteOrigin}${siteBase}`
 
